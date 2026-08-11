b07124a1b2dde7665ed4cc2d5b109498d82ad294
Alisher Umarov <87524321+umarovcoder@users.noreply.github.com>
2026-08-11 14:41:00 +0500

    Add BotLab input component

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,1 @@
+import * as React from "react";import {cn} from "@/lib/utils";export const Input=React.forwardRef<HTMLInputElement,React.ComponentProps<"input">>(({className,...p},ref)=><input ref={ref} className={cn("mt-2 flex h-10 w-full rounded-md border border-input bg-background px-3 text-sm outline-none focus:ring-2 focus:ring-ring",className)} {...p}/>);Input.displayName="Input";
